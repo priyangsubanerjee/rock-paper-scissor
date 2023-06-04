3feeb9bc87b3b5f5090302aa4f96130946c2fd21
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-06-05 00:02:36 +0530

    duration

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="fixed flex flex-col justify-end inset-0 h-full w-full bg-yellow-100 pb-12">
       <div className="flex justify-center">
-        <button className="h-24 w-24 bg-white flex items-center justify-center shadow-xl rounded-full shadow-yellow-300/50 active:shadow-none active:translate-y-1 transition-all">
+        <button className="h-24 w-24 bg-white flex items-center justify-center shadow-xl rounded-full shadow-yellow-300/50 active:shadow-none active:translate-y-1 transition-all duration-100">
           <img
             src="https://cdn-icons-png.flaticon.com/512/4405/4405457.png"
             alt=""
@@ -14,14 +14,14 @@ export default function Home() {
         </button>
       </div>
       <div className="flex mt-2 items-center justify-center space-x-6">
-        <button className="h-24 w-24 bg-white flex items-center justify-center shadow-xl rounded-full shadow-yellow-300/50 active:shadow-none active:translate-y-1 transition-all">
+        <button className="h-24 w-24 bg-white flex items-center justify-center shadow-xl rounded-full shadow-yellow-300/50 active:shadow-none active:translate-y-1 transition-all duration-100">
           <img
             src="https://cdn-icons-png.flaticon.com/512/7083/7083532.png"
             alt=""
             className="h-10"
           />
         </button>
-        <button className="h-24 w-24 bg-white flex items-center justify-center shadow-xl rounded-full shadow-yellow-300/50 active:shadow-none active:translate-y-1 transition-all">
+        <button className="h-24 w-24 bg-white flex items-center justify-center shadow-xl rounded-full shadow-yellow-300/50 active:shadow-none active:translate-y-1 transition-all duration-100">
           <img
             src="https://cdn-icons-png.flaticon.com/512/3153/3153026.png"
             alt=""

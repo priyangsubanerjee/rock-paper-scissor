c0e5a92ecf4b480cffbc8e9505ff36a09a80dc53
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-06-05 15:39:01 +0530

    iterations

--- app/page.js
+++ app/page.js
@@ -22,19 +22,71 @@ export default function Home() {
       beats: "paper",
     },
   ]);
+
+  const [counter, setCounter] = useState(5);
+  const [shuffle, setShuffle] = useState(false);
+  const [iterations, setIterations] = useState(0);
+  const [computersChoice, setComputersChoice] = useState(null);
+  const [usersChoice, setUsersChoice] = useState(null);
+
+  const decide = (computersChoice) => {
+    console.log(computersChoice, usersChoice);
+  };
+
+  useEffect(() => {
+    if (usersChoice !== null) {
+      setShuffle(true);
+      setCounter(0);
+    }
+  }, [usersChoice]);
+
+  useEffect(() => {
+    if (shuffle) {
+      if (iterations > 20) {
+        setIterations(0);
+        setShuffle(false);
+        let random = Math.floor(Math.random() * 3);
+        setComputersChoice(options[random]);
+        setCounter(random);
+        decide(options[random]);
+        return;
+      }
+      const interval = setInterval(() => {
+        if (counter === 2) {
+          setCounter(0);
+        } else {
+          setCounter((prev) => prev + 1);
+        }
+        setIterations((prev) => prev + 1);
+      }, 200);
+      return () => clearInterval(interval);
+    } else {
+      return;
+    }
+  }, [counter, shuffle]);
   return (
     <main className="fixed inset-0 h-full w-full bg-gradient-to-b from-yellow-100 to-yellow-100">
       <div className="h-1/2 relative border-b border-yellow-700/30">
         <div className="absolute top-5 inset-x-0">
           <div className="flex justify-center items-center space-x-3">
-            <button className="h-24 w-24 flex items-center justify-center bg-white rounded-full shadow-lg shadow-yellow-200">
+            <button
+              style={{
+                opacity: counter === 1 ? 1 : 0.5,
+              }}
+              className="h-24 w-24 flex items-center justify-center bg-white rounded-full shadow-lg shadow-yellow-200"
+            >
               <img
                 src="https://cdn-icons-png.flaticon.com/512/7083/7083532.png"
                 className="h-10"
                 alt=""
               />
             </button>
-            <button className="h-24 w-24 flex items-center justify-center bg-white rounded-full shadow-lg shadow-yellow-200">
+            <button
+              style={{
+                opacity: counter === 2 ? 1 : 0.5,
+              }}
+              className="h-24 w-24 flex items-center justify-center bg-white rounded-full shadow-lg shadow-yellow-200"
+            >
               <img
                 src="https://cdn-icons-png.flaticon.com/512/3153/3153026.png"
                 className="h-10"
@@ -43,7 +95,12 @@ export default function Home() {
             </button>
           </div>
           <div className="flex justify-center items-center">
-            <button className="h-24 w-24 flex items-center justify-center bg-white rounded-full shadow-lg shadow-yellow-200">
+            <button
+              style={{
+                opacity: counter === 0 ? 1 : 0.5,
+              }}
+              className="h-24 w-24 flex items-center justify-center bg-white rounded-full shadow-lg shadow-yellow-200"
+            >
               <img
                 src="https://cdn-icons-png.flaticon.com/512/4405/4405457.png"
                 className="h-10"
@@ -52,12 +109,20 @@ export default function Home() {
             </button>
           </div>
         </div>
+
+        {iterations}
         <div className="absolute bottom-0 translate-y-1/2 bg-red-500/50 w-full h-12 inset-x-0"></div>
       </div>
       <div className="h-1/2 relative">
         <div className="absolute bottom-5 inset-x-0">
           <div className="flex justify-center items-center">
-            <button className="h-24 w-24 flex items-center justify-center bg-white rounded-full shadow-lg shadow-yellow-200">
+            <button
+              onClick={() => {
+                setUsersChoice(options[0]);
+              }}
+              disabled={usersChoice !== null && usersChoice !== options[0]}
+              className="h-24 w-24 disabled:opacity-50 flex items-center justify-center bg-white rounded-full shadow-lg shadow-yellow-200 active:shadow-none active:translate-y-1 transition-all duration-100"
+            >
               <img
                 src="https://cdn-icons-png.flaticon.com/512/4405/4405457.png"
                 className="h-10"
@@ -66,14 +131,26 @@ export default function Home() {
             </button>
           </div>
           <div className="flex justify-center items-center space-x-3">
-            <button className="h-24 w-24 flex items-center justify-center bg-white rounded-full shadow-lg shadow-yellow-200">
+            <button
+              onClick={() => {
+                setUsersChoice(options[1]);
+              }}
+              disabled={usersChoice !== null && usersChoice !== options[1]}
+              className="h-24 w-24 disabled:opacity-50 flex items-center justify-center bg-white rounded-full shadow-lg shadow-yellow-200 active:shadow-none active:translate-y-1 transition-all duration-100"
+            >
               <img
                 src="https://cdn-icons-png.flaticon.com/512/7083/7083532.png"
                 className="h-10"
                 alt=""
               />
             </button>
-            <button className="h-24 w-24 flex items-center justify-center bg-white rounded-full shadow-lg shadow-yellow-200">
+            <button
+              onClick={() => {
+                setUsersChoice(options[2]);
+              }}
+              disabled={usersChoice !== null && usersChoice !== options[2]}
+              className="h-24 w-24 disabled:opacity-50 flex items-center justify-center bg-white rounded-full shadow-lg shadow-yellow-200 active:shadow-none active:translate-y-1 transition-all duration-100"
+            >
               <img
                 src="https://cdn-icons-png.flaticon.com/512/3153/3153026.png"
                 className="h-10"
